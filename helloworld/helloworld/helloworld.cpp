65b722138e6bb4301d1f4774710b3aea25ae5f42
tsuneki2 <toru.tsuneki2@gmail.com>
2022-11-09 14:30:33 +0900

    tsuneki2 commit

--- helloworld/helloworld/helloworld.cpp
+++ helloworld/helloworld/helloworld.cpp
@@ -6,15 +6,7 @@
 int main()
 {
 	int a = 10;
-	int b = 30;
-	int c = 50;
-	int d = 70;
-	int e = 90;
 	printf("a = %d\n", a);
-	printf("b = %d\n", b);
-	printf("c = %d\n", c);
-	printf("d = %d\n", d);
-	printf("e = %d\n", e);
 	getchar();
 }
 
