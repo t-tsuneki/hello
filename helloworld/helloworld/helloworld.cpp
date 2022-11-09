bbe0d0e4d5a5ac6b1be2d7a811ec4804a017d5c0
tsuneki <toru.tsuneki@gmail.com>
2022-11-09 15:51:48 +0900

    origin

--- helloworld/helloworld/helloworld.cpp
+++ helloworld/helloworld/helloworld.cpp
@@ -9,10 +9,12 @@ int main()
 	int b = 20;
 	int c = 30;
 	int d = 40;
+	int e = 50;
 	printf("a = %d\n", a);
 	printf("b = %d\n", b);
 	printf("c = %d\n", c);
 	printf("d = %d\n", d);
+	printf("e = %d\n", e);
 
 	getchar();
 }
